a8484d6bc3e6729a09c3e5cbc2ccc2a63f116c20
Elizabeth R <elzaabeth@gmail.com>
2021-11-22 21:53:52 +0530

    Display ContactDetails Implemented

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import user from '../images/user.png'
 import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function ContactDetails(props) {
     const location = useLocation();
@@ -16,6 +17,12 @@ export default function ContactDetails(props) {
                     <div className="header">{name}</div>
                     <div className="description">{email}</div>
                 </div>
+            </div>
+            <div className="ui one column stackable center aligned page grid">
+                <Link to="/">
+                <button className="ui button blue center">
+                        Back to Contact List</button>
+                </Link>
             </div>
         </div>
     )
